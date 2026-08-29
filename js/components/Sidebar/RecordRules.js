d4696f613f566105bd507c9926c592a9a28aea73
Duckly <realactethx@gmail.com>
2026-08-30 02:49:52 +0300

    Update RecordRules.js

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -6,39 +6,25 @@ export default {
             <p>
                 1. You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Note that Click Between Frames is allowed but Physics Bypass is banned.).
             </p>
-            <p>
-                2. Challenges MUST be less than 30 seconds long and AT LEAST 5 seconds long . If It is longer than 30 seconds or shorter than 5 seconds it will not be accepted on the list.
-            </p>
              <p>
-                3. NO 2P challenges allowed.
+                2. NO 2P challenges allowed.
             </p>
             <p>
-                4. Completion must have clicks clearly audible in the video. (Mobile players are exempt from this rule but they should preferably have taps.) Click sounds mod is NOT allowed.
+                3. Completion must have clicks clearly audible in the video. (Mobile players are exempt from this rule but they should preferably have taps.) Click sounds mod is NOT allowed.
             </p>
             <p>
-                5. The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
-            </p>
-             <p>
-                6. Challenges cannot be entirely spam based, (e.g alternation challenges or extremely high CPS spam). Spam Sections in challenges are allowed though with a MAX of 15 CPS.
+                4. The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
             </p>
             <p>
-                7. The recording must also show the player hit the endwall and the endscreen must be shown or the completion will be invalidated.
+                5. The recording must also show the player hit the endwall and the endscreen must be shown or the completion will be invalidated.
             </p>
             <p>
-                8. Challenges that are just copied parts of an existing level are not allowed and won't be accepted. You can take inspiration from other levels but a straight up copied part is not allowed.
+                6. Do not use secret routes, skips, or bug routes!
             </p>
             <p>
-                9. Do not use secret routes, skips, or bug routes!
+                7. Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
             </p>
             <p>
-                10. Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
-            </p>
-            <p>
-                11. To submit a new challenge you MUST be at least level 10 on the discord sever. You can still submit records of levels if you are not level 10.
-            </p>
-            <p>
-                12. Challenges must have some effort put into them. If a challenge lacks enough effort it will get denied.
-            </p>
         </div>
     `
 }
