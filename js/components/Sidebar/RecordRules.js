4da06cd12eb74287ccf9fc42b7101c2f8f5684d6
Duckly <realactethx@gmail.com>
2026-08-30 03:04:26 +0300

    Update RecordRules.js

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -6,23 +6,20 @@ export default {
             <p>
                 1. You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack. Note that Click Between Frames is allowed but Physics Bypass is banned.).
             </p>
-             <p>
-                2. NO 2P challenges allowed.
+            <p>
+                2. Completion must have clicks clearly audible in the video. (Mobile players are exempt from this rule but they should preferably have taps.) Click sounds mod is NOT allowed.
             </p>
             <p>
-                3. Completion must have clicks clearly audible in the video. (Mobile players are exempt from this rule but they should preferably have taps.) Click sounds mod is NOT allowed.
+                3. The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
             </p>
             <p>
-                4. The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
+                4. The recording must also show the player hit the endwall and the endscreen must be shown or the completion will be invalidated.
             </p>
             <p>
-                5. The recording must also show the player hit the endwall and the endscreen must be shown or the completion will be invalidated.
+                5. Do not use secret routes, skips, or bug routes!
             </p>
             <p>
-                6. Do not use secret routes, skips, or bug routes!
-            </p>
-            <p>
-                7. Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
+                6. Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
             </p>
             <p>
         </div>
